6617d15aa1a89fee10221a05ee9b3a2c55ac1346
heloisa-gomes <gomesheloisa999@gmail.com>
2024-08-15 00:12:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let num1 = prompt('Digite um número')
+let num2 = prompt(' Digite outro número')
+
+let soma = parseInt(num1) + parseInt(num2)
+let sub = parseInt(num1) - parseInt (num2)
+let mult = parseInt(num1) * parseInt(num2)
+let div = parseInt(num1) / parseInt(num2)
+console.log('A soma é: ' + soma)
+console.log('A subtração é: ' + sub)
+console.log('A multiplicação é '+ mult)
+console.log('A divisão é: ' + div)
